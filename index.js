25d9cbe8fd89b0b9e2595c68a6e7b7732c5ab35f
Philipp Alferov <philipp.alferov@gmail.com>
2016-09-06 22:51:17 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+'use strict';
+const meow = require('meow');
+const chalk = require('chalk');
+const rfdScrapper = require('rfd-scrapper');
+const cli = meow(`
+  Usage
+    $ rfd-scrapper [--pages=<number>]
+  Flags
+    --limit=<number> Limit number of returned entries
+`);
+const limit = cli.flags.limit || 10;
+
+function getNumberOfPages(limit) {
+  const entriesPerPage = 30;
+  return Math.ceil(limit / entriesPerPage);
+}
+
+rfdScrapper({ limit: getNumberOfPages(limit) })
+  .then((items) => {
+    items
+      .slice(0, limit)
+      .forEach((item, index) => {
+        console.log(chalk.green.bold(item.title));
+        console.log(item.url);
+        console.log(`${item.created_at} \n`);
+      });
+  })
+  .catch(console.error)
